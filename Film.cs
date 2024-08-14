b6439a4d6aadb29699d0fa1f619c1531adacadc9
isimsek7 <isimsekmusic@gmail.com>
2024-08-14 17:56:41 +0300

    Film.cs

---
+++ Film.cs
@@ -0,0 +1,15 @@
+using System;
+namespace coderbyte
+{
+	public class Film
+	{
+		public Film(string ad,double puan)
+		{
+			Ad = ad;
+			Puan = puan;
+		}
+		public string Ad { get; set; }
+		public double Puan { get; set; }
+	}
+
+}
